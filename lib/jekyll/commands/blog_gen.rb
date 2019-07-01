a1169f7ba1b37f4f59d4f5c4dc0997752261d57a
Ceri <ceri@wearecore.co.uk>
2019-07-01 15:25:18 +0100

    Debug

--- lib/jekyll/commands/blog_gen.rb
+++ lib/jekyll/commands/blog_gen.rb
@@ -126,7 +126,7 @@ Jekyll.logger.info 'Made _posts collection directory. Test.'
             Jekyll.logger.info 'Converting featured image UID to local file path'
             # Convert featured image UID to local file path
             if post.has_key?('featured_image')
-              assetData = assets.find {|asset| asset['uid'] == post['featured_image']}
+              assetData = assets.find {|asset| asset['uid'] == post['featured_image'].uid}
 
               if assetData
                 post['featured_image'] = "assets/images/#{post['featured_image']}/#{assetData['filename']}"
